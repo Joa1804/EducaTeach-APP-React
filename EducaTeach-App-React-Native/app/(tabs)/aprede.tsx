7b5f71609f15cd93cff7a60f171fcd0d97b6fa49
João Emanoel <emanuel23pagani76@gmail.com>
2026-09-02 12:35:49 -0300

    Commit.08: mUdado o local do appshel

--- EducaTeach-App-React-Native/app/(tabs)/aprede.tsx
+++ EducaTeach-App-React-Native/app/(tabs)/aprede.tsx
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import AppShell from '../../app/(tabs)/appshell';
+import AppShell from '../../components/appshell';
 
 export default function AprenderScreen() {
   return (
--- EducaTeach-App-React-Native/app/(tabs)/index.tsx
+++ EducaTeach-App-React-Native/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import AppShell from '../../app/(tabs)/appshell';
+import AppShell from '../../components/appshell';
 
 export default function HomeScreen() {
   return (
